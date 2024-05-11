b5b14cdac7c1b7cdc63d860751cff94a0bfa7316
Cauê Vaccari <cauevaccari07@gmail.com>
2024-05-10 22:12:56 -0300

    Valid methods of board

--- Chess/board/Board.cs
+++ Chess/board/Board.cs
@@ -9,7 +9,7 @@ namespace board
 {
     internal class Board
     {
-        public int Rows {  get; set; }
+        public int Rows { get; set; }
         public int Cols { get; set; }
 
         private Piece[,] _pieces;
@@ -18,16 +18,39 @@ namespace board
         {
             Rows = rows;
             Cols = cols;
-            _pieces = new Piece[Rows, Cols]; 
+            _pieces = new Piece[Rows, Cols];
         }
         public Piece GetPiece(int i, int j)
         {
             return _pieces[i, j];
         }
+        public Piece GetPiece(Position p)
+        {
+            return _pieces[p.Row, p.Col];
+        }
         public void SetPiecePosition(Piece p, Position pos)
         {
             _pieces[pos.Row, pos.Col] = p;
             p.position = pos;
+        }
+        public bool PieceExist(Position p)
+        {
+            PositionValid(p);
+            return GetPiece(p) != null;
+        }
+        public bool ValidPosition(Position p)
+        {
+            if (p.Row < 0 || p.Col < 0 || p.Row >= Rows || p.Col >= Cols)
+                return false;
+            return true;
+        }
+        
+        public void PositionValid(Position p)
+        {
+            if (!ValidPosition(p))
+            {
+                throw new BoardException("Invalid position")
+            }
         }
     }
 }
